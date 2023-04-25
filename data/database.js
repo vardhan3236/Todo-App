ba22a8d5f67bda1cdd14eb004945d943e3ba0755
unknown <harshvardhansinha302@gmail.com>
2023-04-26 01:15:20 +0530

    Fixed package.json and config.env

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, { dbName: 'backendapi' })
     .then((c) => {
-        console.log("Database connected");
+        console.log(`Database connected with ${c.connection.host}`);
     })
     .catch((e) => console.log(e));
 }
