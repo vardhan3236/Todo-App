d4a83f52d940508f1b0fbe971cd05ff251adb2ba
unknown <harshvardhansinha302@gmail.com>
2023-04-28 21:33:53 +0530

    changed Max_Age

--- utils/features.js
+++ utils/features.js
@@ -9,7 +9,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
         .cookie(
             'token', token, {
             httpOnly: true,
-            maxAge: 15 * 60 * 1000,
+            maxAge: 10000000,
             sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
             secure: process.env.NODE_ENV === "Development" ? false : true,
         }
